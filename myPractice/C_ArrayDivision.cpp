a525450d49c10182d91c2877f774e0cc259d5ec7
adnin-ropun <adninzahid55@gmail.com>
2025-04-21 00:22:14 +0600

    Updated C_ArrayDivision.cpp, C_ArrayDivision.exe on 04/21/2025 12:22 AM

--- myPractice/C_ArrayDivision.cpp
+++ myPractice/C_ArrayDivision.cpp
@@ -1,14 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isValid(vector<long long>&v,long long m,long long k){
+
+    long long cnt=0,sumContainer=0;
+    for(auto elem:v){
+        if(elem>m) return 0;
+        if((sumContainer+elem) >m ){
+            cnt++;
+            sumContainer=0;
+        }
+        sumContainer+=elem;
+    }
+    if(sumContainer>0) cnt++;
+
+    return cnt<=k;
+
+}
+
 signed main(){
-    int n,k;
+    long long n,k;
+    cin >> n >> k;  
 
-    vector<int> v(n);
-    for(int i=0;i<n;i++) cin>>v[i];
+    vector<long long> v(n);
+    for(long long i=0;i<n;i++) cin>>v[i];
+    
+    long long l=*max_element(v.begin(),v.end());
+    long long r=accumulate(v.begin(),v.end(),0ll);
 
-    int l=*max_element(v.begin(),v.end());
-    cout<<l<<endl;
+    // cout<<isValid(v,8,k);
 
+    long long ans=-1; // Initialize to -1 to indicate "no solution found"
+    while(r>=l){
+        long long m=l+(r-l)/2;
+
+        if(isValid(v,m,k)){
+            r=m-1;
+            ans=m;
+        }
+        else{
+            l=m+1;
+        }
+    }
+    if(ans != -1)
+    cout<<ans<<endl;
+    else cout<<l<<endl;
     return 0;
 }
